b17bb1aceb2e876e6ae73838b32153e134fd0696
ozika <zika.ondra@gmail.com>
2022-08-31 13:16:21 +0000

    added docker

--- R/git_init.R
+++ R/git_init.R
@@ -6,3 +6,14 @@ usethis::use_git_config(
   init.defaultBranch = "main") # <-- not necessary but kinder than 'master'
 
 usethis::use_git()
+usethis::create_github_token(description = "Token for Repro Workshop 2022 Marbach")
+gitcreds::gitcreds_set() 
+usethis::gh_token_help()
+usethis::use_github()
+usethis::use_mit_license()
+usethis::use_readme_rmd()
+
+
+repro::automate()
+repro::use_gha_docker()
+usethis::browse_github_actions()
